1c472a3acf7d69cd33a3d51ac0ada64e865cfda0
Mike Tyan <mike.tyan@yahoo.com>
2016-02-11 16:59:42 -0800

    Uploaded - Finished
    Added Javadoc notes.

--- Ch6/Card.java
+++ Ch6/Card.java
@@ -1,15 +1,28 @@
+/**
+ * The Card class keeps track of the rank and suit of entered card. The 
+ * data is kept private to this class and can only be accessed by methods
+ * provided.
+ */
+
 package javaCh6;
 
 public class Card {
-	
+	// Private instance variables.
 	private int cardSuit;
 	private int cardRank;
-	
+	/**
+	 * Creates a new Card object with the following parameters.
+	 * @param rank Rank of the card ranging from 1 - 13.
+	 * @param suit Suit of the card ranging from 1 - 4.
+	 */
 	public Card(int rank, int suit) {
 		cardRank = rank;
 		cardSuit = suit;
 	}
-	
+	/**
+	 * Gets the rank of card.
+	 * @return The corresponding value as String.
+	 */
 	public String getRank() {
 		switch (cardRank) {
 			case 1: return "Ace";
@@ -19,7 +32,10 @@ public class Card {
 			default : return Integer.toString(cardRank);
 		}
 	}
-	
+	/**
+	 * Gets the suit of card.
+	 * @return The corresponding value as String.
+	 */
 	public String getSuit() {
 		switch (cardSuit) {
 			case 1: return "Spades";
@@ -29,7 +45,10 @@ public class Card {
 			default: return "";
 		}
 	}
-	
+	/**
+	 * Creates a String to identify this card.
+	 * @return The String used to display card information.
+	 */
 	public String toString() {
 		return cardRank + " of " + cardSuit;
 	}
